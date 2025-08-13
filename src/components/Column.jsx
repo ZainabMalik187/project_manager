6c8f728601c81bd147cf347387b29657a048ad73
ZainabMalik187 <zainab.malik0@icloud.com>
2025-08-13 12:59:16 +0500

    Integrated with Droppable

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,17 +1,24 @@
 import PropTypes from "prop-types";
 import TaskCard from "./TaskCard";
 import AddTask from "./AddTask";
-const Column = ({ id, title, cards, onAddTask }) => (
-  <div className="column">
-    <h2>{title}</h2>
-    <AddTask onAdd={(task) => onAddTask(id, task)} />
-    <div className="task-list">
-      {cards.map((task) => (
-        <TaskCard key={task.id} {...task} />
-      ))}
+import { useDroppable } from "@dnd-kit/core";
+
+const Column = ({ id, title, cards, onAddTask }) => {
+  const { setNodeRef } = useDroppable({ id });
+
+  return (
+    <div ref={setNodeRef} className="column">
+      <h2>{title}</h2>
+      <AddTask onAdd={(task) => onAddTask(id, task)} />
+      <div className="task-list">
+        {cards.map((task) => (
+          <TaskCard key={task.id} {...task} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
+
 Column.propTypes = {
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   title: PropTypes.string.isRequired,
@@ -25,6 +32,7 @@ Column.propTypes = {
       priority: PropTypes.string,
     })
   ).isRequired,
-  onAddTask:PropTypes.func.isRequired,
+  onAddTask: PropTypes.func.isRequired,
 };
+
 export default Column;
